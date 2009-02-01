0a8750b9eefe8df471c34662b1c89841489bd15c
Michel <michel.git@pollet.net>
2009-02-01 14:52:45 +0000

    MINI2440: Better peripheral init

--- board/mini2440/mini2440.c
+++ board/mini2440/mini2440.c
@@ -87,18 +87,25 @@ int board_init (void)
 
 	/* set up the I/O ports */
 	gpio->GPACON = 0x007FFFFF;
+#if defined(CONFIG_MINI2440) 
+	gpio->GPBCON = 0x00295551;
+#else
 	gpio->GPBCON = 0x002a9655;
+#endif
 	gpio->GPBUP = 0x000007FF;
+	
 	gpio->GPCCON = 0xAAAAAAAA;
 	gpio->GPCUP = 0x0000FFFF;
 	gpio->GPDCON = 0xAAAAAAAA;
 	gpio->GPDUP = 0x0000FFFF;
+	
 	gpio->GPECON = 0xAAAAAAAA;
 	gpio->GPEUP = 0x0000FFFF;
 	gpio->GPFCON = 0x000055AA;
 	gpio->GPFUP = 0x000000FF;
 	gpio->GPGCON = 0xFD95FFBA;
 	gpio->GPGUP = 0x0000FFFF;
+	
 #ifdef CONFIG_SERIAL3
 	gpio->GPHCON = 0x002AAAAA;
 #else
